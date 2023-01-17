6f8f8696383cab70b05e0aea9c254b3a8dd903a9
jinyoung <sm099@naver.com>
2023-01-17 23:30:32 +0900

    :lipstick: design : 투두 생성 폼 스타일링
    투두 생성 폼 스타일링 완료

---
+++ src/components/molecules/ToDoCreateForm/index.tsx
@@ -0,0 +1,19 @@
+import {Input, Text, Textarea} from '@/components/atoms'
+import {FormLayout} from '@/wrappers'
+import {FormButton} from '@/components/atoms'
+
+const ToDoCreateForm = () => {
+  return (
+    <FormLayout formStyle='py-6 flex flex-col items-center'>
+      <Text textStyle='text-[1.5rem] mb-2'>TO-DO 생성</Text>
+      <Input type='text' placeholder='제목을 입력해주세요.' />
+      <Textarea
+        placeholder='내용을 입력하세요.'
+        textareaStyle='w-[20rem] h-[5rem] resize-none rounded-lg px-2 py-4 text-[12px]'
+      />
+      <FormButton>등록</FormButton>
+    </FormLayout>
+  )
+}
+
+export default ToDoCreateForm
