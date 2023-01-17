f8897b84f601b8e6bfacd5962189d2d34ff44a3a
jinyoung <sm099@naver.com>
2023-01-17 16:08:32 +0900

    :fire: Remove : wrapper 폴더 구조 재 정의를 위한 wrapper 내 layout 파일 삭제
    wrapper 폴더 구조

--- src/wrappers/Input.layout.tsx
+++
@@ -1,8 +0,0 @@
-import {LayoutProps} from '@/types'
-import React from 'react'
-
-const InputLayout = ({children}: LayoutProps) => {
-  return <div className={'w-full flex flex-col items-center'}>{children}</div>
-}
-
-export default InputLayout
