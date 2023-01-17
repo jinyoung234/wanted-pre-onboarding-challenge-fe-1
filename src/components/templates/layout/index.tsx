344ddf721640c436a76674f5c6fd2b53e6f1c801
jinyoung <sm099@naver.com>
2023-01-17 18:34:38 +0900

    :lipstick: design : 루트 레이아웃 컴포넌트 로그인 & 회원 가입 페이지 스타일 오류 수정
    로그인 & 회원 가입 페이지에서 가운데가 아닌 상단에 위치하는 오류 수정

--- src/components/templates/layout/index.tsx
+++ src/components/templates/layout/index.tsx
@@ -1,3 +1,5 @@
+import {classNames} from '@/utils'
+import {useRouter} from 'next/router'
 import React from 'react'
 
 interface LayoutProps {
@@ -5,7 +7,9 @@ interface LayoutProps {
 }
 
 function Layout({children}: LayoutProps) {
-  return <div className='max-w-xl mx-auto h-screen bg-slate-100 flex flex-col'>{children}</div>
+  const {pathname} = useRouter()
+  const AuthStyle = pathname !== '/' && classNames('justify-center')
+  return <div className={`max-w-xl mx-auto h-screen bg-slate-100 flex flex-col ${AuthStyle}`}>{children}</div>
 }
 
 export default Layout
