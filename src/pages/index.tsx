92057dc75e05fc8fb5bcef650af3d6bcaba587a7
jinyoung <sm099@naver.com>
2023-01-19 02:50:43 +0900

    :lipstick: design : 메인 페이지 스타일링
    메인 페이지 스타일링 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import {ToDoBoard, ToDoList} from '@/components/organisms'
 import {useUser} from '@/hooks'
+import {Dimmed, ToDoLayout} from '@/wrappers'
 import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
   useUser()
-  return <div>home</div>
+  return (
+    <ToDoLayout>
+      <ToDoList />
+      <Dimmed />
+      <ToDoBoard todo={{createAt: '2022-01-17', title: '끝내주게 쉬기', content: '낮잠을 자며 쉰다'}} />
+    </ToDoLayout>
+  )
 }
 export default Home
