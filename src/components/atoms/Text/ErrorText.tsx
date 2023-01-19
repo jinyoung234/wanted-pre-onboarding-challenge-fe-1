46215895dedfdb74e80b92df19d98b719d83a72b
jinyoung <sm099@naver.com>
2023-01-19 19:43:24 +0900

    :lipstick: design : ErrorText props에 extraStyle 추가
    유동적인 스타일링을 위한 extraStyle 추가 및 적용

--- src/components/atoms/Text/ErrorText.tsx
+++ src/components/atoms/Text/ErrorText.tsx
@@ -1,9 +1,12 @@
+import {classNames} from '@/utils'
+
 interface ErrorTextProps {
   errorContent: string
+  extraStyle?: string
 }
 
-const ErrorText = ({errorContent}: ErrorTextProps) => {
-  return <p className='w-[20rem] text-red-500 text-[12px]'>{errorContent}</p>
+const ErrorText = ({errorContent, extraStyle}: ErrorTextProps) => {
+  return <p className={`w-[20rem] text-red-500 text-[12px] ${extraStyle && classNames(extraStyle)}`}>{errorContent}</p>
 }
 
 export default ErrorText
