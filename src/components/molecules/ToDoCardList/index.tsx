70f0f8e9b7d0988e212ba8351af86b944456d3ef
jinyoung <sm099@naver.com>
2023-01-19 19:26:03 +0900

    :sparkles: feat : 투두 조회 기능 완료
    투두 조회 기능 완료

--- src/components/molecules/ToDoCardList/index.tsx
+++ src/components/molecules/ToDoCardList/index.tsx
@@ -1,10 +1,14 @@
-import {DUMMY_TODOCARD} from '@/constants'
+import {useGetTodos} from '@/queries'
+import React from 'react'
 import ToDoCard from '@/components/molecules/ToDoCard'
+import {Spinner} from '@/components/atoms'
 
 const ToDoCardList = () => {
+  const {todos, isLoading} = useGetTodos()
+  if (isLoading) return <Spinner />
   return (
     <section className='w-full flex flex-col items-center justify-center'>
-      {DUMMY_TODOCARD.map((todo, _) => (
+      {todos.map((todo, _) => (
         <ToDoCard todo={todo} key={_} />
       ))}
     </section>
