816eccea7c8563b395d4ada2571ca13efbb0bb82
jinyoung <sm099@naver.com>
2023-01-19 19:25:35 +0900

    :wrench: add : useGetTodos export 방식 변경
    queries/index.ts에 useGetTodos export 추가 하는 방식으로 변경

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,2 +1,3 @@
 export {default as useSignUpMutation} from '@/queries/auth/useSignUpMutation'
 export {default as useSignInMutation} from '@/queries/auth/useSignInMutation'
+export {default as useGetTodos} from '@/queries/main/useGetTodos'
